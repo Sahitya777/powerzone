3b6cc22834bff66eb44be206a4053c95fe9cb650
Sahitya77 <sahityanijhawan@gmail.com>
2022-06-16 21:29:52 +0530

    search exercises

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 import {Routes,Route} from 'react-router-dom'
-
+import { Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Footer from './components/Footer';
 function App() {
   return (
-    <div>
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path='/exercise/:id' element={<ExerciseDetail/>}/>
       </Routes>
       <Footer/>
-    </div>
+    </Box>
   );
 }
 
