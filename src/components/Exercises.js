c51139629a162a2fc65fb6f2fba81116ac66b49c
Sahitya77 <sahityanijhawan@gmail.com>
2022-06-17 21:47:39 +0530

    exercises component updated

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Pagination,Box,Stack,Typography } from '@mui/material'
+import {exercisesOptions,fetchData} from '../utils/fetchData'
 
-function Exercises() {
+const Exercises=({exercises,setExercises,bodyPart})=> {
   return (
-    <div>Exercises</div>
+    <Box id="exercises"
+      sx={{mt:{lg:'110px'}}}
+      mt="50px"
+      p="20px"
+    >
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+
+    </Box>
   )
 }
 
