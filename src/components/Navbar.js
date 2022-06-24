344eccc327289a6a04d98ea9fbdf222d8b347bf5
Sahitya77 <sahityanijhawan@gmail.com>
2022-06-24 18:54:08 +0530

    navbar updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,6 +19,7 @@ function Navbar() {
         gap="40px"
         fontSize="24px"
         alignitem="flex-end"
+        
       >
         <Link to="/" style={{textDecoration:'none',color:'#3A1212',borderBottom:'3px solid #FF2625'}}>Home</Link>
         <a href='#exercises' style={{textDecoration:'none',color:'#3A1212'}}>Exercises</a>
