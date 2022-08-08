4b1e1ec7e0428bee0f7f04d95f76b24df257aa7b
Sahitya77 <sahityanijhawan@gmail.com>
2022-08-08 12:27:11 +0530

    minor css updates

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -34,7 +34,7 @@ function HeroBanner() {
         >
             Exercise
         </Typography>
-        <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+        <img src={HeroBannerImage}  alt="banner" className="hero-banner-img" />
     </Box>
   )
 }
